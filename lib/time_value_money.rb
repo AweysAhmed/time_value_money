1adcc08aabbd8a03eae43108d460ccb9865bca50
OFFICE <office@OFFICEs-MacBook-Air-2.local>
2020-01-26 22:53:33 -0500

    working on testing for gem file

---
+++ lib/time_value_money.rb
@@ -0,0 +1,22 @@
+require "time_value_money/version"
+require "bigdecimal"
+
+
+module TimeValueMoney
+  class Error < StandardError; end
+  
+  def self.future_value(interest, years, cost)
+    interestPlusOne = 1 + (interest / 100.0)
+    fv = cost * (interestPlusOne **years)
+    return fv.round(2)
+  end
+
+
+
+def self.present_value(f_value, interest, years)
+  interestPlusOne = 1 + (interest / 100.0)
+  pv = f_value / (interestPlusOne**years)
+  return pv.round(2)
+end
+
+end
---
+++ lib/time_value_money/version.rb
@@ -0,0 +1,3 @@
+module TimeValueMoney
+  VERSION = "0.1.0"
+end
